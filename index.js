a3cd9334f504a47719d86aee3cf16e824430d1a5
david2407 <davidcortes2407@gmail.com>
2022-08-01 23:30:25 -0500

    set app listen

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const personRouter = require('./routes/person')
+const express = require("express");
+const app = express();
+const port = 3000;
+const personRouter = require("./routes/person");
 
-app.use(express.json())
+app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended:true
-    })
-)
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/', (req, res)=> {
-res.json({ message: "ok"})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
 
-app.use('/person', personRouter)
+app.use("/person", personRouter);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
+  const statusCode = err.statusCode || 500;
   console.error(err.message, err.stack);
   res.status(statusCode).json({ message: err.message });
   return;
-})
+});
 
-app.listen(port, ()=>{
-    console.log(`app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 3000 ,function(){ console.log("up and running on port "+process.env.PORT); });
